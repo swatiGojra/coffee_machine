6cfa9f82aaf9d94124c0d014c17ad5ca978a87de
swatiGojra <69855165+swatiGojra@users.noreply.github.com>
2020-08-30 21:37:31 +0530

    Update Main.java
    Remove testing code

--- coffeeMachine/src/main/java/io/dunzo/coffeeMachine/Main.java
+++ coffeeMachine/src/main/java/io/dunzo/coffeeMachine/Main.java
@@ -14,26 +14,5 @@ public class Main
 {
 	public static void main(String[] args) throws Exception {
 
-
-		String fileName = "src/main/resources/input1.txt";
-		CoffeeService instance = new CoffeeServiceImpl();
-		CreateCoffeeMachineRequest coffeeMachineRequest = FileUtil
-				.readFile(fileName, CreateCoffeeMachineRequest.class);
-		instance.createCoffeeMachine(coffeeMachineRequest);
-
-		List<String> requests = Arrays.asList("espresso", "hot_water", "espresso", "hot_milk", "espresso");
-		instance.serveRequests(requests);
-
-		//wait for all the threads to get executed
-		Thread.sleep(10000);
-
-		//then refill to 1000 units
-		//instance.refillIngredient("chocolate_syrup", 1000);
-
-		//now it can be fulfilled
-		//List<String> requests2 = Arrays.asList("mocha");
-		//instance.serveRequests(requests2);
-
-
 	}
 }
